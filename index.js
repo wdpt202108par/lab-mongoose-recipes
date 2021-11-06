9c144a5256406d4662e760e12cb22080a67874e1
kthomas <kevin.allan.thomas@gmail.com>
2021-11-06 16:12:10 +0100

    Iteration 2

--- index.js
+++ index.js
@@ -21,6 +21,11 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    // insérer notre Recip
+    const data = { "title": "tarte aux pommes", ingredients:['pommes', 'pates brisée']}
+    Recipe.create(data)
+    .then(recipe => console.log('la tarte est prête', recipe))
+    .catch(error => console.log("error lors de l'insertion d'un recip"))
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
