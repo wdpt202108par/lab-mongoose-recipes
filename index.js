b19e4b0535cc63198cd6383c802ec9acca2ef237
Yooyoo56 <tak.yoojeong@gmail.com>
2021-11-06 15:50:52 +0100

    iteration 2 done

--- index.js
+++ index.js
@@ -7,11 +7,13 @@ const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
+
+//const recipeTitle = new Schema ({title: 'Our recipe'});
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
-    useNewUrlParser: true,
+    useNewUrlParser: true,                                                                                       
     useUnifiedTopology: true
   })
   .then(self => {
@@ -19,7 +21,20 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
+  // deuxieme then 
   .then(() => {
+    //Need to write the Recipe.create(); to access the base de donnees
+    Recipe.create({})
+    .then( () => {
+      console.log(`the title of the recipe is: ${Recipe.title}`);
+      // on insert une recipt dans la bdd
+      // console.log ...
+    }).catch((error) => {
+      console.log(`Error lors de l'insertion de recipe`, error)
+
+    })
+
+   // console.log(`The title of the recipe is :${}`);
     // Run your code here, after you have insured that the connection was made
   })
   .catch(error => {
