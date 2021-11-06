202a086c996739b4c56ca7ba022dd2c02241fb64
Lea Beaulieu <beaulieu.lea@free.fr>
2021-11-06 15:49:49 +0100

    iteration 1 done

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,7 +2,34 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+    title: { String, required: true, unique: true },
+    level: {
+        type: String,
+        enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"],
+    },
+
+    ingredients: [String],
+
+    cuisine: { String },
+    dishType: {
+        type: String,
+        enum: ["Main_course", "Soup", "Snack", "Drink", "Dessert", "Other"],
+    },
+
+    image: {
+        type: String,
+        default: 'https://images.media-allrecipes.com/images/75131.jpg',
+    },
+
+    duration: { type: Number, min: 0 },
+
+    creator: { type: String },
+
+    created: {
+        type: Date,
+        default: new Date()
+    },
+
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
