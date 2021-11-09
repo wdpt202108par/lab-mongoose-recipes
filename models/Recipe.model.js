9ca4d2c992a615ea11d68bc69b9ccc03a27f3b88
kthomas <kevin.allan.thomas@gmail.com>
2021-11-09 21:37:40 +0100

    Lab done from Iteration 1 to 3

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+//
+// ITERATION 1
+//
+
 const recipeSchema = new Schema({
   title: String,
   level: {type: String, enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef']},
