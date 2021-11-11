77b110c2c3a7a3694e0d3177b78023d3f036de6d
Lea Beaulieu <beaulieu.lea@free.fr>
2021-11-11 15:50:28 +0100

    done until iteration 5 + brainstorming iteration 6

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
-// Import of the data from './data.json'
-const data = require('./data');
+
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
@@ -14,17 +13,54 @@ mongoose
         useNewUrlParser: true,
         useUnifiedTopology: true
     })
-    .then(self => {
-        console.log(`Connected to the database: "${self.connection.name}"`);
-        // Before adding any recipes to the database, let's remove all existing ones
-        return Recipe.deleteMany()
-    })
-    .then(() => {
+
+.then(self => {
+    console.log(`Connected to the database: "${self.connection.name}"`);
+    // Before adding any recipes to the database, let's remove all existing ones
+    return Recipe.deleteMany()
+})
+
+.then(() => {
         // Run your code here, after you have insured that the connection was made
-        //       Recipe.create({ title: "tarte aux pommes" })
-        //         .then(recipe => { console.log("insértion réussie", recipe) })
-        //       .catch(error => console.log("erreur insertion"))
+
+
+        // ITERATION 2 : creer une première recette lambda
+        Recipe.create({ title: "tarte aux pommes", ingredients: ["topping iteration 2", "pomme"] })
+
+        // 2nd promesse liée à l'insertion de la première recette
+        .then(recipe => { console.log("SUCCESS : insertion réussie de la première recette", recipe) })
+            .catch(error => console.log("ERROR :  insertion de la première recette", recipe))
+
+
+        // ITERATION 3 : ajoute l'intégralité des recettes dans la db depuis le fichier importé data.json
+
+        // Import of the data from './data.json'
+        const data = require('./data');
+        Recipe.insertMany(data)
+            .then(recipe => console.log("SUCCESS : insertions réussies de toutes les recettes du fichier importé data", 'title', recipe))
+            .catch(error => console.log("ERROR : insertion des recettes depuis data"))
+
+        // ITERATION 4 : ajoute l'intégralité des recettes dans la db depuis le fichier importé data.json
+        // avec var mais ne fonctionne pas (const recipeToUpdate= "Rigatoni alla Genovese")
+        Recipe.findOneAndUpdate({ title: 'Rigatoni alla Genovese' }, { duration: 100 })
+            .then(function(majRecipe) {
+                console.log("SUCCESS : mise à jour réussie de la recette", majRecipe)
+            })
+            .catch(error => console.log("ERROR : maj erreur recette", majRecipe))
+
+        // ITERATION 5 : suppression du carrot caaake
+        Recipe.deleteOne({ title: 'Carrot Cake' })
+            .then(function(deleteRecipe) {
+                console.log("SUCCESS : suppression réussie de la recette", deleteRecipe)
+            })
+            .catch(error => console.log("ERROR : suppression recette", deleteRecipe))
+            // DB promesse si connexion db all bad
     })
     .catch(error => {
         console.error('Error connecting to the database', error);
-    });
+    })
+
+/* ITERATION 6 : couper connexion mongoose après execution de toutes les opérations
+BRAINSTORMING : gérer le timing d'éxecution des opérations avec PROMISES ALL ?
++     mongoose.connection.close();
+*/
